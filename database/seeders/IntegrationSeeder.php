78cb0bbaa1d1e3b0b0c8bc57d6c7d1ac34119506
German Kalinec <gkalinec@gmail.com>
2025-09-19 18:22:36 -0700

    Small update to take off the google integrator
    Signed-off-by: German Kalinec <gkalinec@gmail.com>

--- database/seeders/IntegrationSeeder.php
+++ database/seeders/IntegrationSeeder.php
@@ -6,7 +6,6 @@ use App\Classes\Integrators\IntegrationsManager;
 use App\Classes\Integrators\Local\LocalIntegrator;
 use App\Models\Integrations\Integrator;
 use App\Models\Utilities\SchoolRoles;
-use halestar\FabLmsGoogleIntegrator\GoogleIntegrator;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\App;
 
@@ -20,7 +19,7 @@ class IntegrationSeeder extends Seeder
         //register the integrations here.
 	    $manager = App::make(IntegrationsManager::class);
 		$manager->registerIntegrator(LocalIntegrator::class, true);
-		$manager->registerIntegrator(GoogleIntegrator::class, false);
+		//$manager->registerIntegrator(GoogleIntegrator::class, false);
 		//Assign basic permissions
 	    foreach(Integrator::all() as $integrator)
 			$integrator->assignRole([SchoolRoles::$EMPLOYEE, SchoolRoles::$STUDENT, SchoolRoles::$PARENT]);
